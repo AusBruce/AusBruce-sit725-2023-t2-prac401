c1884a2402f26a9ac2c4bd4dc6a1642f6528a560
stephenliu8964 <dq89_64@hotmail.com>
2023-08-03 16:35:50 +1000

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ let app = express();
 let port = process.env.port || 3000;
 
 app.use(express.static(__dirname + '/'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', function (req,res) {
     res.render('index.html');
